b3fafa2096b91aafa7e396bade9a6cc8079bd669
hamare1207 <110402773+hamare1207@users.noreply.github.com>
2026-05-22 13:12:16 +0900

    class

--- js/app.js
+++ js/app.js
@@ -1,29 +1,19 @@
 console.log("開始");
 
-const animal1 = {
-  "0": "ネズミ",
-  "1": "ネコ",
-  "2": "イヌ",
-  length: 3
-};
+class Animal {
+  constructor(type, name) {
+    this.type = type;
+    this.name = name;
+  }
+  getInf() {
+    return `${this.type} : ${this.name}`;
+  }
+}
 
-console.log(Array.isArray(animal1), animal1);
+const cat = new Animal("ネコ", "タマ");
+const dog = new Animal("イヌ", "ポチ");
 
-const animal2 = Array.from(animal1);
-
-console.log(Array.isArray(animal2), animal2);
-
-const animal3 = ["nezumi", ["neko", "inu", 333], "kuma"];
-
-console.log(Array.isArray(animal3), animal3);
-
-
-const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-
-const sum = arr.reduce((a, x) => {
-  return a + x;
-}, 0);
-
-console.log(arr, sum);
+console.log(cat.getInf());
+console.log(dog.getInf());
 
 console.log("終了");
