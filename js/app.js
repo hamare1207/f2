c30ab17eea3fac6dcf5ca45aa37221784512ee74
hamare1207 <110402773+hamare1207@users.noreply.github.com>
2026-05-22 11:57:38 +0900

    array

--- js/app.js
+++ js/app.js
@@ -1,20 +1,29 @@
 console.log("開始");
 
-const animal = {
-  name : "",
-  age : 0,
-  getInf(){
-    return `${this.name}(${this.age}歳)`;
-  },
-  get inf() {
-    return this.getInf();
-  },
-  set month(x){
-    this.age = Math.trunc(x / 12);
-  }
-}
+const animal1 = {
+  "0": "ネズミ",
+  "1": "ネコ",
+  "2": "イヌ",
+  length: 3
+};
 
-animal.name = "タマ";
-animal.month = 26;
-console.log(animal.inf);
-console.log(animal.getInf());
+console.log(Array.isArray(animal1), animal1);
+
+const animal2 = Array.from(animal1);
+
+console.log(Array.isArray(animal2), animal2);
+
+const animal3 = ["nezumi", ["neko", "inu", 333], "kuma"];
+
+console.log(Array.isArray(animal3), animal3);
+
+
+const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+const sum = arr.reduce((a, x) => {
+  return a + x;
+}, 0);
+
+console.log(arr, sum);
+
+console.log("終了");
